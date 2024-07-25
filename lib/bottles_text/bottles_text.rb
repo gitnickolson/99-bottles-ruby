f8e565362eb8e1e84887974882c3da8612bfa24f
gitnickolson <nicknickolson@web.de>
2024-07-25 11:28:57 +0200

    Remove oneline syntax from `BottlesText`

--- lib/bottles_text/bottles_text.rb
+++ lib/bottles_text/bottles_text.rb
@@ -4,14 +4,35 @@ class BottlesText
   def initialize(number) = @number = number
   attr_reader :number
 
-  def self.for(number) = bottles_text_classes.find { |candidate| candidate.handles?(number) }&.new(number)
+  def self.for(number)
+    bottles_text_classes.find { |candidate| candidate.handles?(number) }&.new(number)
+  end
 
-  def bottles = "#{number} bottles"
-  def next_bottle_number = BottlesText.for(number - 1)
-  def third_line = 'Take one down, pass it around,'
-  def to_s = bottles
+  def bottles
+    "#{number} bottles"
+  end
 
-  def self.handles?(_number) = true
-  def self.bottles_text_classes = @bottles_text_classes ||= [self]
-  def self.register(candidate) = bottles_text_classes.prepend(candidate)
+  def next_bottle_number
+    BottlesText.for(number - 1)
+  end
+
+  def third_line
+    'Take one down, pass it around,'
+  end
+
+  def to_s
+    bottles
+  end
+
+  def self.handles?(_number)
+    true
+  end
+
+  def self.bottles_text_classes
+    @bottles_text_classes ||= [self]
+  end
+
+  def self.register(candidate)
+    bottles_text_classes.prepend(candidate)
+  end
 end
